2ac4d67af4d6ec70ba579b51d80e2122e5b8de69
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-12-03 21:18:50 +0300

    Cделана программа

--- HW1.5.2/Program.cs
+++ HW1.5.2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите число");
+            bool isParsed = Int32.TryParse(Console.ReadLine(), out int index);
+            if (isParsed)
+            {
+               int result = 1;
+               int multiplier = 1;
+               while (result / 10 != index)
+               {
+                    result = index * multiplier;
+                    Console.WriteLine($"{index}x{multiplier}={result}");
+                    multiplier += 1;
+               }
+            }
+            else
+            {
+                Console.WriteLine("Попробуйте ввести число");
+            }
+
+            Console.ReadKey();
         }
     }
 }
