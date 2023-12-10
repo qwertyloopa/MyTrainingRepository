50a73fcd45970f400f2200962edacea3d7c2a226
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-12-11 00:23:57 +0300

    сделано дз по массивам(блок-схему по 1 заданию вышлю отдельно)

---
+++ HW1.6.1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HW1._6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void ArrayAndString(int[] arr, string str) 
+        {
+            
+        }
+    }
+}
