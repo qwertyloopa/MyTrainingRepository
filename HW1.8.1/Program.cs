277179cae645bb1bde751e9e892df2b57473d93c
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2023-12-18 21:20:07 +0300

    сделано 1 задание

---
+++ HW1.8.1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace HW1._8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item item = new Item();
+
+            item.GetName();
+            Console.WriteLine("Введите имя");
+            item.SetName(Console.ReadLine());
+            item.GetName();
+        }
+
+        class Item
+        {
+            private string Name = "unknown";
+
+            public void SetName(string name)
+            {
+                Name = name;
+            }
+
+            public void GetName()
+            {
+                Console.WriteLine(Name);
+            }
+        }
+    }
+}
