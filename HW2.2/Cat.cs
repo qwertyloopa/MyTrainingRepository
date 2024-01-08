35be970f4b1c4a668171958010eed44497045902
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-08 23:38:40 +0300

    1 и 2 задания в тупике

---
+++ HW2.2/Cat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2._2
+{
+    internal class Cat : Animal, ICarnivore
+    {
+        string Sound = "Meow";
+        string Color { set => Console.ReadLine(); }
+        string ICarnivore.typeC { get; set; }
+        string ICarnivore.foodC { get; set; }
+
+
+        public Cat(string sound, string color)
+        {
+            Sound = sound;
+            Color = color;
+        }
+
+        public override void displayAnimal()
+        {
+            Console.WriteLine("I'm a cat");
+        }
+
+        public void DisplayEatC()
+        {
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ HW2.2/ICarnivore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2._2
+{
+    internal interface ICarnivore
+    {
+        public abstract string typeC { get; set; }
+        public abstract string foodC { get; set; }
+
+        public void DisplayEatC();
+    }
+}
