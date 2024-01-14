4e2941a654d249b9e8eca939fd284f1d3feab22e
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-14 19:51:03 +0300

    добавлена предпоследняя домашка

---
+++ HW2.3.1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace HW2._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var word in FilterWords(GenerateRandomWords(3)))
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static IEnumerable<string> FilterWords(IEnumerable<string> words)
+        {
+            foreach (var word in words)
+            {
+                if (word.Length > 5)
+                {
+                    yield return word;
+                }
+            }
+        }
+
+        static IEnumerable<string> GenerateRandomWords(int count)
+        {
+
+            Dictionary<int, string> dictionary = new Dictionary<int, string>
+            {
+                { 0, "словарь" },
+                { 1, "генератор" },
+                { 2, "метод" },
+                { 3, "программа" },
+                { 4, "компьютер" },
+                { 5, "информация" },
+                { 6, "технология" },
+                { 7, "разработка" },
+                { 8, "алгоритм" },
+                { 9, "структура" }
+            };
+
+            var random = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                int index = random.Next(dictionary.Count);
+                yield return dictionary[index];
+            }
+        }
+    }
+}
---
+++ HW2.3.2/ComparableListData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2._3._2
+{
+    internal class ComparableListData : IComparable<ComparableListData>
+    {
+        public int IdNumber { get; set; }
+        public string Name { get; set; }
+
+        public ComparableListData(int idNumber, string name)
+        {
+            IdNumber = idNumber;
+            Name = name;
+        }
+
+        public int CompareTo(ComparableListData other)
+        {
+            if (other == null) return 1;
+            return IdNumber.CompareTo(other.IdNumber);
+        }
+    }
+
+    public class SuperCollection<T> : List<T> where T : IComparable<T>
+    {
+        public new void Add (T item)
+        {
+            base.Add(item);
+            Sort();
+        }
+    }
+}
