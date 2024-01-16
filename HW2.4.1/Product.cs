8314a4ab7c6d95421d140d6071fdfa7f9e83eb72
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-16 17:24:20 +0300

    сделано 1 задание

---
+++ HW2.4.1/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW2._4._1
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public Product(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
