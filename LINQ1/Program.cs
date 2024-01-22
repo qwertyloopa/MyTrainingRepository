2247669931cc59420720b8eaa7a56b0a02858f5a
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-22 23:29:34 +0300

    Сделано 1 задание

---
+++ LINQ1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LINQ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = {-10, 22, 13, 43, -5, -12, 100};
+
+            IEnumerable<int> ints2 = from i in ints
+                                     where i > 0 
+                                     select i;
+            var result = ints2.Reverse().ToList();
+            foreach (int r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
