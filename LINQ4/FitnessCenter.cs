61bffa45740c40fba3577be0cacec90da4e903fe
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-26 22:38:26 +0300

    Дописано 4 задание

---
+++ LINQ4/FitnessCenter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ4
+{
+    internal class FitnessCenter
+    {
+        public int year;
+        public int month;
+        public int duration;
+        public int clientCode;
+
+        public FitnessCenter(int year, int month, int duration, int clientCode)
+        {
+            this.year = year;
+            this.month = month;
+            this.duration = duration;
+            this.clientCode = clientCode;
+        }
+    }
+}
---
+++ LINQ4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LINQ4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FitnessCenter> trainings = new List<FitnessCenter>();
+            trainings.Add(new FitnessCenter(2004, 12, 2, 1337));
+            trainings.Add(new FitnessCenter(2008, 9, 4, 1488));
+            trainings.Add(new FitnessCenter(2004, 5, 5, 1677));
+            trainings.Add(new FitnessCenter(2004, 5, 2, 1991));
+            trainings.Add(new FitnessCenter(2013, 12, 6, 1337));
+            trainings.Add(new FitnessCenter(2004, 12, 2, 1337));
+            trainings.Add(new FitnessCenter(2008, 6, 2, 1337));
+            trainings.Add(new FitnessCenter(2004, 7, 2, 1337));
+            Console.WriteLine("Я еблан и это не обсуждается");
+            
+            List<FitnessCenter> fitnesses = new List<FitnessCenter>();
+            foreach(FitnessCenter fitness in trainings)
+            {
+                var result = trainings.OrderByDescending(x => x.duration).ThenByDescending(x => x.year).FirstOrDefault();
+                fitnesses.Add(new FitnessCenter(result.year, result.month, result.duration, result.clientCode));
+                Console.WriteLine($"{result.year}, {result.month}, {result.duration}, {result.clientCode}");
+            }
+        }
+    }
+}
