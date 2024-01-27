8d33c6982f71ae560b61224b3a021eab503c6c98
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-27 20:31:36 +0300

    первая попытка

---
+++ LINQ5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LINQ5
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string>{"Hello", "My", "World"};
+           
+
+            Console.WriteLine(strings.Median());
+        }
+
+        static string Median(this List<string> s)
+        {
+            string result = s.ElementAt((System.Index)Math.Round((decimal)s.Count()/2));
+            return result;
+        }
+    }
+}
