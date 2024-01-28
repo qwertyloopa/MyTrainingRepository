65b6d0d5b319043aed26584c2e7dcc1c02980a60
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-29 01:36:23 +0300

    Сделано 1 задание по Файлам

---
+++ Files1/Directories.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files1
+{
+    internal class Directories
+    {
+        public string FilePath;
+
+        public Directories(string path)
+        {
+            FilePath = path;
+        }
+
+        public static int GetCountOfFile(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            int count = dir.GetFiles().Length;
+            return count;
+        }
+
+        public static List<FileInfo> GetFileInfo(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            List<FileInfo> filesInfo = new List<FileInfo>();
+            foreach(string f in files)
+            {
+                filesInfo.Add(new FileInfo(f));
+            }
+            return filesInfo;
+        }
+    }
+}
