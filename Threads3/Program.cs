3194c9fa676147d125696609ed0f820367906457
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-01-30 03:45:07 +0300

    Сделано 3 задание (многопоточность)

---
+++ Threads3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Threads3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            var squares = new List<int>();
+
+            Parallel.ForEach(nums, x =>
+            {
+                var square = x ^ 2;
+                squares.Add(square);
+                Console.WriteLine($"Квадрат числа {x} равен {square}");
+            });
+        }
+    }
+}
