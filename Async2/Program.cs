16b5ab75cf6ea710675c62f416a3c550f97c1d42
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-02-04 03:53:07 +0300

    Сделано 1 задание

---
+++ Async2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async2
+{
+
+    internal class Program
+    {
+        public static int time;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            time = Int32.Parse(Console.ReadLine());
+            Callback();
+            Console.WriteLine("Квадрат данного числа равен: {0}", time^2);
+            Console.ReadKey();
+        }
+        
+        static async void Callback()
+        {
+            Task taskMain = Task.Run(() => { 
+                for (int i = 0; i < time; i++) 
+                { 
+                    Console.WriteLine("Hello from callback");
+                }
+            });
+            
+            var awaiter = taskMain.GetAwaiter();
+            
+            awaiter.IsCompleted(()=>
+            {
+                Console.WriteLine("");
+            });
+        }
+    }
+}
