99d0a62899fc676b8677ab93859c619bfb9cb1fc
David Kaim <57368063+qwertyloopa@users.noreply.github.com>
2024-02-12 02:42:01 +0300

    ну и последнее по эксепшенам

---
+++ HW2.3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HW2._3._3;
+
+public class Program {
+    static void Main(string[] args)
+    {
+        var stringProcessor = new StringProcessor();
+        
+        try
+        {
+            stringProcessor.ProcessString("");
+        } catch (EmptyStringException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        
+    }
+}
